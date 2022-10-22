317ad4c8354995e1c810d0695420c61106570a81
QaziTalha4595 <97889460+QaziTalha4595@users.noreply.github.com>
2022-10-22 18:08:58 +0530

    yesyy

---
+++ js/countdown-custom.js
@@ -0,0 +1,7 @@
+(function($) {	
+	'use strict';
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2022, 12-1, 16, 0)}); // year, month, date, hour
+        });
+})(jQuery);
+
